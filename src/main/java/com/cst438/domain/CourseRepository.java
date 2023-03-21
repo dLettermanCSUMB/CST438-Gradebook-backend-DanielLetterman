46e0c1f564033c9100db989a41464a074f0b4ca3
dLetterman <danielcletterman@yahoo.com>
2023-03-21 16:23:38 -0700

    GradeBookController.java: Modified createAssignment to function with Frontend and switching from PutMapping to PostMapping Modified deleteAssignment to DeleteMapping from PostMapping Added getInstructorCourses function as a GetMapping for "/createAssignment"
    CourseRepository.java:
    Added findByInstructor query function for use with getInstructorCourses function
    
    Added CourseListDTO.java for providing a list of courses.

--- src/main/java/com/cst438/domain/CourseRepository.java
+++ src/main/java/com/cst438/domain/CourseRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface CourseRepository extends CrudRepository <Course, Integer> {
     @Query("select c from Course c where c.course_id= :course_id and c.instructor= :email")
     List<Course> findByInstructorAndId(@Param("email") String email, @Param("course_id") int course_id);
+
+    @Query("select c from Course c where c.instructor= :email")
+    List<Course> findByInstructor(@Param("email") String email);
 }
